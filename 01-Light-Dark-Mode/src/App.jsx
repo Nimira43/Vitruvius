b681302bf9b26342fbe1835e22c8213c4e4588f9
Nimira43 <n.rabone72@aol.com>
2025-07-03 00:19:59 +0100

    on click toggle dark mode

--- 01-Light-Dark-Mode/src/App.jsx
+++ 01-Light-Dark-Mode/src/App.jsx
@@ -20,6 +20,7 @@ const App = () => {
   return ( 
     <div className='min-h-screen bg-light relative'>
       <button 
+        onClick={toggleDarkMode}
         className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 lg:w-10 h-9 lg:h-10 flex justify-center items-center rounded-full text-[#fffcfa] bg-[#008080] shadow-lg hover:bg-[#2e5252] hover-transition cursor-pointer'
       >
         <TbMoon className='text-lg lg:text-xl' />
