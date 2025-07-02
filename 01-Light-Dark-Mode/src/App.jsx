d65beda2a795161151b244892be93a70f55bb3a6
Nimira43 <n.rabone72@aol.com>
2025-07-02 23:16:40 +0100

    Styling toggle button. Added hover-transition class in custom CSS

--- 01-Light-Dark-Mode/src/App.jsx
+++ 01-Light-Dark-Mode/src/App.jsx
@@ -2,7 +2,13 @@ import Hero from './components/Hero'
 
 const App = () => {
   return ( 
-    <div className='min-h-screen bg'>
+    <div className='min-h-screen bg-light relative'>
+      <button 
+        className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 lg:w-10 h-9 lg:h-10 flex justify-center items-center rounded-full bg-support shadow-lg hover:bg-dark'
+      
+      >
+
+      </button>
       <Hero />
     </div>
   )
