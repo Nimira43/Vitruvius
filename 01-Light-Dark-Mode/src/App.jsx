95b957b86a04e49136231b05c8427a3d08a5fc3d
Nimira43 <n.rabone72@aol.com>
2025-07-02 23:21:32 +0100

    Uodated

--- 01-Light-Dark-Mode/src/App.jsx
+++ 01-Light-Dark-Mode/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return ( 
     <div className='min-h-screen bg-light relative'>
       <button 
-        className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 lg:w-10 h-9 lg:h-10 flex justify-center items-center rounded-full bg-support shadow-lg hover:bg-dark'
+        className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 lg:w-10 h-9 lg:h-10 flex justify-center items-center rounded-full bg-support shadow-lg hover:bg-dark hover-transition cursor-pointer'
       
       >
 
