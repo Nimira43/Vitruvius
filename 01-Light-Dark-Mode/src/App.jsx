4186437a3cd5d79cec735d38c8ca042599d761b2
Nimira43 <n.rabone72@aol.com>
2025-07-03 00:35:31 +0100

    Setting button bg colour for light and dark modes.

--- 01-Light-Dark-Mode/src/App.jsx
+++ 01-Light-Dark-Mode/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     <div className='min-h-screen bg-[#fffcfa] text-[#111] dark:bg-[#111] dark:text-[#fffcfa] relative'>
       <button 
         onClick={toggleDarkMode}
-        className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 lg:w-10 h-9 lg:h-10 flex justify-center items-center rounded-full text-[#fffcfa] bg-[#008080] shadow-lg hover:bg-[#2e5252] hover-transition cursor-pointer'
+        className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 lg:w-10 h-9 lg:h-10 flex justify-center items-center rounded-full text-[#fffcfa] bg-[#ff4500] hover:bg-[#b44403] dark:bg-[#008080]  dark:hover:bg-[#2e5252] hover-transition cursor-pointer'
       >
         <TbMoon className='text-lg lg:text-xl' />
       </button>
