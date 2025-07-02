0621ed5353d1779862b2a1fe9f9acb31527a4682
Nimira43 <n.rabone72@aol.com>
2025-07-02 23:49:28 +0100

    Styled moon icon

--- 01-Light-Dark-Mode/src/App.jsx
+++ 01-Light-Dark-Mode/src/App.jsx
@@ -5,9 +5,9 @@ const App = () => {
   return ( 
     <div className='min-h-screen bg-light relative'>
       <button 
-        className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 lg:w-10 h-9 lg:h-10 flex justify-center items-center rounded-full text-[#fffcfa] bg-[#008080] shadow-lg hover:bg-[#111] hover-transition cursor-pointer'
+        className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 lg:w-10 h-9 lg:h-10 flex justify-center items-center rounded-full text-[#fffcfa] bg-[#008080] shadow-lg hover:bg-[#2e5252] hover-transition cursor-pointer'
       >
-        
+        <TbMoon className='text-lg lg:text-xl' />
       </button>
       <Hero />
     </div>
